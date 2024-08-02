7e68eed3682fa4925a4909e7cf33c471a6fd3549
Sai Pruthvik <saipruthvik07@gmail.com>
2024-08-02 08:45:32 +0530

    refactored fetch for error handling

--- src/helpers/Fetch.ts
+++ src/helpers/Fetch.ts
@@ -1,23 +1,42 @@
+import { FailureResponse } from "../types/TmdbTypes";
 import { CONSTANTS } from "./Constants";
 
 export async function fetchApi<T>(
-  url = "",
-  methodType = "GET",
-  data = undefined
+  endPoint = "",
+  methodType: "GET" | "POST" | "PUT" | "DELETE" | "PATCH" = "GET",
+  requestBody?: unknown
 ): Promise<T> {
-  const completeUrl = new URL(CONSTANTS.ENV.TMDB_API_BASE_URL + url);
+  const completeUrl = new URL(CONSTANTS.ENV.TMDB_API_BASE_URL + endPoint);
   completeUrl.searchParams.set("api_key", CONSTANTS.ENV.TMDB_API_KEY);
-  const response = await fetch(completeUrl, {
-    method: methodType,
-    body: JSON.stringify(data),
-  });
-  return response.json() as T;
-}
 
-export const METHOD_TYPE = {
-  GET: "GET",
-  POST: "POST",
-  PUT: "PUT",
-  DELETE: "DELETE",
-  PATCH: "PATCH",
-};
+  const options: RequestInit = {
+    method: methodType,
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+
+  if (
+    requestBody &&
+    (methodType === "POST" || methodType === "PUT" || methodType === "PATCH")
+  ) {
+    options.body = JSON.stringify(requestBody);
+  }
+
+  try {
+    const response = await fetch(completeUrl.toString(), options);
+    const data = await response.json();
+
+    if (!response.ok) {
+      const errorMessage =
+        (data as FailureResponse).status_message || "Unknown error occurred";
+      console.log(errorMessage);
+      throw new Error(errorMessage);
+    }
+
+    return data as T;
+  } catch (error: unknown) {
+    console.error(`Error Fetching Data: ${error}`);
+    throw new Error(`Failed to fetch data`);
+  }
+}
